0fec4de00ef2b0cf7a2b41ef5f9fb4c405eefca8
bernard deffarges <bernard@llaama.com>
2023-09-22 12:11:26 +0200

    fixed localhost for container

--- src/main/scala/org/linthaal/LinthaalSupervisor.scala
+++ src/main/scala/org/linthaal/LinthaalSupervisor.scala
@@ -44,7 +44,7 @@ object LinthaalSupervisor {
     // Akka HTTP still needs a classic ActorSystem to start
     import system.executionContext
 
-    val futureBinding = Http().newServerAt("localhost", 8080).bind(routes)
+    val futureBinding = Http().newServerAt("0.0.0.0", 8080).bind(routes)
     futureBinding.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
