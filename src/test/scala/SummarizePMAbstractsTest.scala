5d3e0d39412facd8a17dbb03aad0013b2a664792
mvelimir <velimirmilinkovic1@gmail.com>
2024-01-03 09:54:27 +0100

    Format test files

--- src/test/scala/SummarizePMAbstractsTest.scala
+++ src/test/scala/SummarizePMAbstractsTest.scala
@@ -1,36 +1,28 @@
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import org.linthaal.api.routes.PubMedAISumReq
-import org.linthaal.tot.pubmed.{PubMedSumAct, PubMedToTManager}
-import org.linthaal.tot.pubmed.PubMedToTManager.{ActionPerformed, AllSummarizationRequests, RetrieveAll, StartAISummarization}
+import org.linthaal.tot.pubmed.{ PubMedSumAct, PubMedToTManager }
+import org.linthaal.tot.pubmed.PubMedToTManager.{ ActionPerformed, AllSummarizationRequests, RetrieveAll, StartAISummarization }
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.concurrent.duration.DurationInt
 
-/**
+/** This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+  * by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
   *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version. 
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
   *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-  *
+  * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
   */
 
 class SummarizePMAbstractsTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 
   "AI to summarize abstracts based on a query " must {
     val timeout = 30.seconds
-    //#test
+    // #test
     " reply with a list of summarized abstracts. " in {
-    val replyAP = createTestProbe[ActionPerformed]()
-    val replyRes = createTestProbe[AllSummarizationRequests]()
+      val replyAP = createTestProbe[ActionPerformed]()
+      val replyRes = createTestProbe[AllSummarizationRequests]()
       val underTest = spawn(PubMedToTManager())
       val passumReq = PubMedAISumReq("pancreatic cancer biomarkers")
       underTest.tell(StartAISummarization(passumReq, replyAP.ref))
@@ -40,7 +32,3 @@ class SummarizePMAbstractsTest extends ScalaTestWithActorTestKit with AnyWordSpe
     }
   }
 }
-
-
-
-
